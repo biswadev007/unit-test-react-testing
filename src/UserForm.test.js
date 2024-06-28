0036e935dd5dab95699876fbc239103d4793c3d0
Biswajit Paul <134674632+biswajit-dev-009@users.noreply.github.com>
2024-06-28 13:07:59 +0530

    Get elemnt by label

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -23,7 +23,13 @@ test('it will check the form value and passes on submit of form', () => {
 
   render(<UserForm onUserAdd={mock} />);
 
-  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+  // const [nameInput, emailInput] = screen.getAllByRole('textbox');
+  const nameInput = screen.getByRole('textbox', {
+    name: /name/i
+  });
+  const emailInput = screen.getByRole('textbox', {
+    name: /email/i
+  });
 
   userEvent.click(nameInput);
   userEvent.keyboard(name);
